689a6cb45c8254caebee63962dec383720cdc042
n0nz <nonthawat.duangchai@gmail.com>
2020-09-15 20:48:08 +0700

    update #9

---
+++ 09 - Parallax Scrolling/start.js
@@ -0,0 +1,16 @@
+(() => {
+  function onScroll() {
+    const moonElem = document.querySelector('.moon');
+    const wishElem = document.querySelector('.wish');
+
+    // console.log(window.scrollY);
+
+    moonElem.style.transform = `translate(${window.scrollY * 0.7}%, ${window.scrollY * -0.7}%)`;
+    wishElem.style.transform = `translateY(${window.scrollY * -1.2}%)`;
+  }
+
+  function run() {
+    document.addEventListener('scroll', onScroll);
+  }
+  run();
+})();
