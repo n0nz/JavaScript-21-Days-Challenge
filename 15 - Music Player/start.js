4d2fd2a308bd9159c2011851dd6c4596afa50e4f
n0nz <nonthawat.duangchai@gmail.com>
2020-09-22 22:51:56 +0700

    update #15

---
+++ 15 - Music Player/start.js
@@ -0,0 +1,57 @@
+(() => {
+  const audioElem = document.querySelector('audio');
+  const playBtnElem = document.querySelector('.play');
+  const progressBarElem = document.querySelector('.progress-bar');
+  const startTimeElem = document.querySelector('.start-time');
+  const endTimeElem = document.querySelector('.end-time');
+
+  function onClick() {
+    if (audioElem.paused) {
+      audioElem.play();
+      playBtnElem.className = 'pause';
+    } else {
+      audioElem.pause();
+      playBtnElem.className = 'play';
+    }
+  }
+
+  function getDuration(time) {
+    const minute = Math.floor(time / 60 % 60).toString();
+    const second = Math.floor(time % 60).toString().padStart(2, '0');
+
+    return `${minute}:${second}`;
+  }
+
+  function onTimeUpdate() {
+    // console.log(audioElem.currentTime);
+    startTimeElem.innerHTML = getDuration(audioElem.currentTime);
+    progressBarElem.value = audioElem.currentTime;
+  }
+
+  function onLoadedData() {
+    // console.log(audioElem.duration);
+    endTimeElem.innerHTML = getDuration(audioElem.duration);
+    progressBarElem.max = audioElem.duration;
+  }
+
+  function onInput() {
+    audioElem.currentTime = progressBarElem.value;
+  }
+
+  function onEnded() {
+    playBtnElem.className = 'play';
+    audioElem.currentTime = 0;
+  }
+
+  function run() {
+    playBtnElem.addEventListener('click', onClick);
+
+    audioElem.addEventListener('timeupdate', onTimeUpdate);
+    audioElem.addEventListener('loadeddata', onLoadedData);
+    audioElem.addEventListener('ended', onEnded);
+
+    progressBarElem.addEventListener('input', onInput);
+  }
+
+  run();
+})();
